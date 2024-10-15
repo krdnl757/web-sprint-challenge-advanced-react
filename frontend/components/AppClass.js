8da33edf85f9c898ffce86624612c69155128112
krdnl757 <143224517+krdnl757@users.noreply.github.com>
2024-10-14 21:02:37 -0400

    Update AppClass.js

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -21,36 +21,71 @@ export default class AppClass extends React.Component {
   // You can delete them and build your own logic from scratch.
 
   getXY = () => {
+    const { index } = this.state;
+    const x = (index % 3) + 1;
+    const y = Math.floor(index / 3) + 1;
+    return { x, y };
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
   }
 
   getXYMessage = () => {
+    const { x, y } = this.getXY();
+    return `Coordinates (${x}, ${y})`;
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
   }
 
   reset = () => {
+    this.setState({
+      message: initialMessage,
+      email: initialEmail,
+      index: initialIndex,
+      steps: initialSteps,
+    });
     // Use this helper to reset all states to their initial values.
   }
 
   getNextIndex = (direction) => {
+    const { index } = this.state;
+  const moves = {
+    left: index % 3 !== 0 ? index - 1 : index,
+    right: index % 3 !== 2 ? index + 1 : index,
+    up: index > 2 ? index - 3 : index,
+    down: index < 6 ? index + 3 : index,
+  };
+  return moves[direction];
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
   }
 
   move = (evt) => {
+    const direction = evt.target.id;
+    const nextIndex = this.getNextIndex(direction);
+    if (nextIndex !== this.state.index) {
+      this.setState(prevState => ({
+        index: nextIndex,
+        steps: prevState.steps + 1,
+        message: ''
+      }));
+    } else {
+      this.setState({ message: "You can't move in that direction" });
+    }
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
   }
 
   onChange = (evt) => {
+    this.setState({ email: evt.target.value });
     // You will need this to update the value of the input.
   }
 
   onSubmit = (evt) => {
+    evt.preventDefault();
+    // Make a POST request (could use fetch or axios for example)
+    console.log("Submitting:", this.state.email);
     // Use a POST request to send a payload to the server.
   }
 
